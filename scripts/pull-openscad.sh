8d1f2a1ee8d64d66442e1d0bf593de7a5174116f
IngeniousKink <ingeniouskink@gmail.com>
2024-06-15 20:48:17 +0200

    Fix download of openscad-wasm

--- scripts/pull-openscad.sh
+++ scripts/pull-openscad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=$(curl -s https://api.github.com/repos/DSchroer/openscad-wasm/releases/latest | jq ".assets[].browser_download_url" -r)
+FILES=$(curl -L -s https://api.github.com/repos/DSchroer/openscad-wasm/releases/latest | jq ".assets[].browser_download_url" -r)
 for FILE in $FILES
 do
     wget $FILE
