c5b4bd5a92f6ae8d8e53c4001a9505427f2044e0
IngeniousKink <ingeniouskink@gmail.com>
2024-06-15 21:40:07 +0200

    Simplify stuff

--- Makefile
+++ Makefile
@@ -1,16 +1,10 @@
-build: configurator/configurator.js configurator/configurator.worker.js configurator/openscad.wasm configurator/openscad.wasm.js
+build: configurator/openscad.wasm configurator/openscad.wasm.js
 
 configurator/openscad.wasm: lib
 	cp lib/openscad.wasm $@
 
 configurator/openscad.wasm.js: lib
 	cp lib/openscad.wasm.js $@
-
-configurator/configurator.worker.js: src/* lib node_modules
-	npx rollup src/configurator.worker.js --file $@ --format iife
-
-configurator/configurator.js: src/* lib node_modules
-	npx rollup src/configurator.js --file $@ --format iife
 
 run: lib node_modules
 	npx http-server configurator
--- configurator/configurator.js
+++ configurator/configurator.js
@@ -80,6 +80,86 @@ export function STLViewer(elem, model) {
   });
 }
 
-window.onload = function() {
-   STLViewerEnable("stlviewer");
-} 
+const form = document.getElementById("configurator");
+const downloadBtn = document.getElementById("download_button");
+const previewBtn = document.getElementById("preview_button");
+const cancelBtn = document.getElementById("cancel");
+const spinnerImg = document.getElementById("spinner");
+const preview = document.getElementById('preview');
+
+let worker;
+
+cancelBtn.onclick = (e) => {
+  e.preventDefault();
+
+  if(worker){
+    worker.terminate();
+  }
+
+  spinnerImg.hidden = true;
+  previewBtn.disabled = false;
+  downloadBtn.disabled = false;
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  triggerWorker(renderFile);
+}
+
+previewBtn.onclick = (e) => {
+  e.preventDefault();
+  triggerWorker(renderFile);
+}
+
+downloadBtn.onclick = (e) => {
+  e.preventDefault();
+  triggerWorker(downloadFile);
+}
+
+const triggerWorker = async (callback) => {
+  worker = new Worker("./configurator.worker.js");
+
+  worker.onmessage = function (e) {
+    callback(e.data);
+    spinnerImg.hidden = true;
+    generateBtn.disabled = false;
+    worker.terminate();
+  };
+
+  spinnerImg.hidden = false;
+  previewBtn.disabled = true;
+  downloadBtn.disabled = true;
+
+  const values = new FormData(form);
+  worker.postMessage({
+    pitch: values.get("pitch"),
+    teeth: values.get("teeth"),
+    thickness: values.get("thickness"),
+    boreDiameter: values.get("bore-diameter"),
+  });
+};
+
+function downloadFile(output) {
+
+  const fileName = 'generated.stl';
+
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(
+    new Blob([output], { type: "application/octet-stream" }),
+  );
+  link.download = fileName;
+  document.body.append(link);
+  link.click();
+  link.remove();
+}
+
+function renderFile(output) {
+  const url = URL.createObjectURL(
+    new Blob([output], { type: "application/octet-stream" }),
+  );
+
+  STLViewer(preview, url);
+}
+
+
+
